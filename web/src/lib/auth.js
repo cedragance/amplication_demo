2221a87a4237ebec8c222f5044acc149c39fcaa8
Dragan Krstic <dragank@gmail.com>
2024-01-22 13:41:15 +0100

    gql updates?

--- web/src/lib/auth.js
+++ web/src/lib/auth.js
@@ -1,5 +1,8 @@
 import { createUrl, get, isStoredJwt, post, setStoredJwt } from "./http";
 
+import { gql, isStoredJwt, setStoredJwt, client } from "./apollo";
+
+/*
 export const me = async () => {
   return isStoredJwt()
     ? (await get(createUrl("/api/me")).catch(() => null))?.data
@@ -26,6 +29,70 @@ export const signup = async (username, password) => {
       () => null
     )
   )?.data;
+
+  if (!result) {
+    return alert("Could not sign up");
+  }
+  setStoredJwt(result.accessToken);
+  return me();
+};
+*/
+const GET_ME = gql`
+  query me {
+    me {
+      id
+    }
+  }
+`;
+
+export const me = async () => {
+  return isStoredJwt()
+    ? (await client.query({ query: GET_ME }).catch(() => null))?.data.me
+    : null;
+};
+
+const LOGIN = gql`
+  mutation login($credentials: Credentials!) {
+    login(credentials: $credentials) {
+      accessToken
+    }
+  }
+`;
+
+export const login = async (username, password) => {
+  const result = (
+    await client
+      .mutate({
+        mutation: LOGIN,
+        variables: { credentials: { username, password } },
+      })
+      .catch(() => null)
+  )?.data.login;
+
+  if (!result) {
+    return alert("Could not login");
+  }
+  setStoredJwt(result.accessToken);
+  return me();
+};
+
+const SIGNUP = gql`
+  mutation signup($credentials: Credentials!) {
+    signup(credentials: $credentials) {
+      accessToken
+    }
+  }
+`;
+
+export const signup = async (username, password) => {
+  const result = (
+    await client
+      .mutate({
+        mutation: SIGNUP,
+        variables: { credentials: { username, password } },
+      })
+      .catch(() => null)
+  )?.data.signup;
 
   if (!result) {
     return alert("Could not sign up");
--- web/src/lib/tasks.js
+++ web/src/lib/tasks.js
@@ -1,6 +1,9 @@
 import qs from "qs";
 import { createUrl, get, patch, post } from "./http";
 
+import { gql, client } from "./apollo";
+
+/*
 export const create = async (text, uid) => {
   const result = (
     await post(createUrl("/api/tasks"), {
@@ -39,6 +42,108 @@ export const update = async (task) => {
       completed: !task.completed,
     }).catch(() => null)
   )?.data;
+
+  if (!result) {
+    return alert("Could not update task");
+  }
+
+  return result;
+};
+*/
+
+const CREATE_TASK = gql`
+  mutation createTask($data: TaskCreateInput!) {
+    createTask(data: $data) {
+      completed
+      createdAt
+      id
+      text
+    }
+  }
+`;
+
+export const create = async (text, uid) => {
+  const result = (
+    await client
+      .mutate({
+        mutation: CREATE_TASK,
+        variables: {
+          data: {
+            completed: false,
+            text,
+            uid: { id: uid },
+          },
+        },
+      })
+      .catch(() => null)
+  )?.data.createTask;
+
+  if (!result) {
+    return alert("Could not create task");
+  }
+
+  return result;
+};
+
+const GET_TASKS = gql`
+  query tasks($where: TaskWhereInput, $orderBy: [TaskOrderByInput!]) {
+    tasks(where: $where, orderBy: $orderBy) {
+      completed
+      createdAt
+      id
+      text
+    }
+  }
+`;
+
+export const getAll = async (uid) => {
+  const result = (
+    await client
+      .query({
+        query: GET_TASKS,
+        variables: {
+          where: { uid: { id: uid } },
+          orderBy: { createdAt: "Asc" },
+        },
+      })
+      .catch(() => null)
+  )?.data.tasks;
+
+  if (!result) {
+    alert("Could not get tasks");
+    return [];
+  }
+
+  return result;
+};
+
+const UPDATE_TASK = gql`
+  mutation updateTask($data: TaskUpdateInput!, $where: TaskWhereUniqueInput!) {
+    updateTask(data: $data, where: $where) {
+      completed
+      createdAt
+      id
+      text
+    }
+  }
+`;
+
+export const update = async (task) => {
+  const result = (
+    await client
+      .mutate({
+        mutation: UPDATE_TASK,
+        variables: {
+          data: {
+            completed: !task.completed,
+          },
+          where: {
+            id: task.id,
+          },
+        },
+      })
+      .catch(() => null)
+  )?.data.updateTask;
 
   if (!result) {
     return alert("Could not update task");
